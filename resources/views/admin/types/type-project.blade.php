45372e68f2f6c9a9699c572448133cd37a8ebd88
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-12-01 17:00:55 +0100

    [edit] List project type

--- resources/views/admin/types/type-project.blade.php
+++ resources/views/admin/types/type-project.blade.php
@@ -2,46 +2,45 @@
 
 @section('content')
 
-    <h1 class="fw-bold text-primary">Index Types</h1>
+    <h1 class="fw-bold text-primary">List Project by Type</h1>
 
-    <form class="col-5" action="{{ route('admin.types.store') }}" method="POST">
-        @csrf
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="New Type" name="name">
-            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Create</button>
-        </div>
-    </form>
-
-    <table class="table table-dark w-75">
-        <thead>
-          <tr>
-            <th scope="col">Type</th>
-            <th scope="col">Project</th>
-          </tr>
-        </thead>
-        <tbody>
-
+    <div class="cont-type bg-dark w-50 p-5">
+        <h4 class="text-white">List Type</h4>
+        <div class="accordion accordion-flush border-bottom border-white" id="accordionFlushExample">
             @foreach ($types as $type)
-                <tr>
-                    <td>
-                        {{ $type->name }}
-                    </td>
-
-                    <td>
+                <div class="accordion-item bg-dark text-white">
+                    <h2 class="accordion-header" id="flush-headingOne-{{$type->id}}">
+                        <button
+                        class="accordion-button collapsed bg-dark text-white"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#flush-collapseOne-{{$type->id}}"
+                        aria-expanded="false"
+                        aria-controls="flush-collapseOne-{{$type->id}}">
+                            {{$type->name}}
+                        </button>
+                    </h2>
+                    <div
+                    id="flush-collapseOne-{{$type->id}}"
+                    class="accordion-collapse collapse"
+                    aria-labelledby="flush-headingOne-{{$type->id}}"
+                    data-bs-parent="#accordionFlushExample">
                         <ul>
-
                             @foreach ($type->projects as $project)
-                            <li>
-                                {{ $project->name }}
-                                <a class="btn btn-success" href="{{ route('admin.projects.show', $project) }}">Go to Details</a>
-                            </li>
+                                <li class="my-3 d-flex justify-content-between">
+                                    {{ $project->name }}
+
+                                    <a class="btn btn-success btn-sm" href="{{ route('admin.projects.show', $project) }}">Go to Details</a>
+                                </li>
                             @endforeach
                         </ul>
-                    </td>
-                </tr>
+                    </div>
+                </div>
+
+
             @endforeach
-        </tbody>
-    </table>
+        </div>
+    </div>
 
 
 @endsection
